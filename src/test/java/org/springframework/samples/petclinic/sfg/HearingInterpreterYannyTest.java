57740691389ce7c7148e15b44c86e59cd8ebb3df
Sam Lau <samlau.sam@gmail.com>
2020-08-14 13:20:33 +0800

    146 assignment

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.sfg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.*;
+
+@RunWith(SpringRunner.class) //from Spring freamework
+@ContextConfiguration(classes={BaseConfig.class, YannyConfig.class})
+public class HearingInterpreterYannyTest {
+	@Autowired
+	HearingInterpreter hearingInterpreter;
+	
+	@Test
+	public void testWhatIheard() {
+		String word = hearingInterpreter.whatIheard();
+		assertEquals("Yanny!", word);
+	}
+
+}
