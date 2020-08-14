0b6df87c699eab259809d6bda7db79ecd9a23452
Sam Lau <samlau.sam@gmail.com>
2020-08-14 13:08:57 +0800

    Using JUnit4 with Spring Context

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -2,17 +2,18 @@ package org.springframework.samples.petclinic.sfg;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.*;
 
+@RunWith(SpringRunner.class) //from Spring freamework
+@ContextConfiguration(classes={BaseConfig.class, LaurelConfig.class})
 public class HearingInterpreterTest {
+	@Autowired
 	HearingInterpreter hearingInterpreter;
 	
-	@Before
-	public void setUp() throws Exception {
-		hearingInterpreter = new HearingInterpreter(new LaurelWordProducer());
-	}
-
 	@Test
 	public void testWhatIheard() {
 		String word = hearingInterpreter.whatIheard();
